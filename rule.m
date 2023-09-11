2c780ea9381f660461b0fc432fccfb82d6a6d049
Curran Robertson <cjr5480@gmail.com>
2023-09-10 21:10:02 -0600

    update

--- rule.m
+++
@@ -1,7 +0,0 @@
-function [if,body] = rule1()
-
-  if_state = Z(i,j) == 1 && j > 1
-body = Z(i+1,j+1) = 1; Z(i+1,j-1) = 1; Z(i+1,j) = 1;
-
-
-end
--- rule1.m
+++
@@ -1,7 +0,0 @@
-function [statement,body] = rule1()
-
-statement = 'Z(i,j) == 1 && j > 1';
-body = 'Z(i,j+1) = 1';
-
-
-end
